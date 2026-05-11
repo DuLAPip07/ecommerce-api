604babdc0820c17365f470f0e13aa223edd9cd68
Oghenekevwe Jeffery Onodje <dulapip28856@gmail.com>
2026-05-11 14:41:32 +0100

    Add user authentication with JWT

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
+app.use('/api/auth', require('./routes/authRoutes'));
 
 app.get('/', (req, res) => {
   res.json({ message: 'E-Commerce API is running!' });
